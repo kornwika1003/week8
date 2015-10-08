19696a9fb88d448d0b65e51bcc792998e34d2f6d
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-10-08 19:47:01 +0700

    Exercise 1

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Test {
+    public static void main(String[] args) {
+        A a = new A();
+        a.print();
+    }
+}
+class A {
+    String s;
+    A(String newS) {
+        s = newS;
+    }
+    public void print() {
+        System.out.print(s);
+    }
+}
